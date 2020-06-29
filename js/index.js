78473cb4b4bbecc09f0da4859e165be10c535d88
Cris Stanza <crisstanza@users.noreply.github.com>
2020-06-29 14:23:28 -0300

    Additional space / note direction.

--- js/index.js
+++ js/index.js
@@ -1,32 +1,41 @@
 (function() {
 
-	let SPACE_SIZE = 5;
+	let SPEED = 60;
+
+	let SPACE_SIZE = 6;
 	let MEASURE_COUNT = 4;
 	let MEASURE_SIZE = 100 / MEASURE_COUNT;
 
 	let MEASURE_BEATS = 4;
 	let BEAT_SIZES = MEASURE_SIZE / (MEASURE_BEATS + 1);
 
-	let POSSIBLE_NOTES_COUNT = 11;
+	let POSSIBLE_NOTES_COUNT = 15;
+	let EXTRA_LINES_LIMITS = [1, 13];
 
-	let SPEED = 60;
-	let METRO;
-	let CREATOR;
-	let NOTES;
+	let START_Y_2 = 50 + (50 - SPACE_SIZE * 4) / 2;
+	let START_Y_1 = (50 - SPACE_SIZE * 4) / 2;
+
+	let LINE_STROKE_WIDTH = 2;
+	let BAR_STROKE_WIDTH = 1;
+	let NOTE_STROKE_WIDTH = 2;
+
+	let METRO, CREATOR;
+	let NOTES, EXTRA_LINES;
 
 	function drawBars(startY) {
-		let y1 = startY + SPACE_SIZE*0 + '%';
-		let y2 = startY + SPACE_SIZE*4 + '%';
+		let delta = SPACE_SIZE/4;
+		let y1 = startY + SPACE_SIZE*0 - delta + '%';
+		let y2 = startY + SPACE_SIZE*4 + delta + '%';
 		for (let i = 0 ; i < MEASURE_COUNT ; i++) {
 			let x = MEASURE_SIZE * (i + 1) + '%';
-			CREATOR.create.svg('line', {x1: x, y1: y1, x2: x, y2: y2, stroke: 'gray'}, svg);
+			CREATOR.create.svg('line', {x1: x, y1: y1, x2: x, y2: y2, stroke: 'gray', 'stroke-width': BAR_STROKE_WIDTH}, svg);
 		}
 	}
 
 	function drawLines(startY) {
 		for (let i = 0 ; i < 5 ; i++) {
 			let y = startY + SPACE_SIZE*i + '%';
-			CREATOR.create.svg('line', {x1: 0, y1: y, x2: '100%', y2: y, stroke: 'black'}, svg);
+			CREATOR.create.svg('line', {x1: 0, y1: y, x2: '100%', y2: y, stroke: 'black', 'stroke-width': LINE_STROKE_WIDTH}, svg);
 		}
 		drawBars(startY);
 	}
@@ -83,8 +92,8 @@
 
 	function init(event) {
 		CREATOR = io.github.crisstanza.ElementsCreator;
-		drawLines(15);
-		drawLines(65);
+		drawLines(START_Y_1);
+		drawLines(START_Y_2);
 		let callback = {
 			willStart: function(beat) {
 				drawNotes();
@@ -112,38 +121,61 @@
 		if (NOTES) {
 			NOTES.forEach(function(note) { note.remove(); });
 		}
+		if (EXTRA_LINES) {
+			EXTRA_LINES.forEach(function(line) { line.remove(); });
+		}
 		for (let k = 0 ; k < MEASURE_COUNT ; k++) {
 			let startX = BEAT_SIZES * k * (MEASURE_BEATS + 1);
 			for (let i = 0 ; i < MEASURE_BEATS ; i++) {
 				let j = Math.floor(Math.random() * POSSIBLE_NOTES_COUNT);
 				let startY, fillColor, line, filled;
 				if (i == 0) {
-					startY = 65;
+					startY = START_Y_2;
 					fillColor = '#FFF';
 					line = false;
 					filled = false;
 				} else {
-					startY = 15;
+					startY = START_Y_1;
 					fillColor = '#333';
 					line = true;
 					filled = true;
 				}
 				let cx = startX + BEAT_SIZES*(i + 1) + '%';
-				let cy = (startY - 2.5) + (SPACE_SIZE/2)*j + '%';
+				let cy = (startY - SPACE_SIZE*1.5) + (SPACE_SIZE/2)*j;
 				let y1 = cy;
-				let y2 = (startY - 12.5) + (SPACE_SIZE/2)*j + '%';
 				let group = CREATOR.create.svg('g', {name: i + k * MEASURE_BEATS + 1, stroke: 'black', fill: fillColor, filled: filled}, svg);
-				let circle = CREATOR.create.svg('ellipse', {cx: cx, cy: cy, ry: '2%', 'stroke-width': 3}, group);
+				let circle = CREATOR.create.svg('ellipse', {cx: cx, cy: cy + '%', ry: SPACE_SIZE/2.5 + '%', 'stroke-width': NOTE_STROKE_WIDTH}, group);
+				let circleRadius = circle.getBoundingClientRect().width / 2;
+				let svgWidth = svg.getBoundingClientRect().width;
+				let deltaX = (circleRadius * 100) / svgWidth;
 				if (line) {
-					let circleRadius = circle.getBoundingClientRect().width / 2;
-					let svgWidth = svg.getBoundingClientRect().width;
-					let delta = (circleRadius * 100) / svgWidth;
-					let x = startX + delta + BEAT_SIZES*(i + 1) + '%';
-					CREATOR.create.svg('line', {x1: x, y1: y1, x2: x, y2: y2, 'stroke-width': 3}, group);
+					let x, y2;
+					if (j < POSSIBLE_NOTES_COUNT / 2) {
+						x = startX - deltaX + BEAT_SIZES*(i + 1) + '%';
+						y2 = (startY + SPACE_SIZE*3) + (SPACE_SIZE/2)*(j - 4) + '%';
+					} else {
+						x = startX + deltaX + BEAT_SIZES*(i + 1) + '%';
+						y2 = (startY - SPACE_SIZE*4) + (SPACE_SIZE/2)*j + '%';
+					}
+					CREATOR.create.svg('line', {x1: x, y1: y1 + '%', x2: x, y2: y2, 'stroke-width': NOTE_STROKE_WIDTH}, group);
 				}
+				if (j <= EXTRA_LINES_LIMITS[0] || j >= EXTRA_LINES_LIMITS[1]) {
+					let x1 = startX + BEAT_SIZES*(i + 1) - deltaX*2 + '%';
+					let x2 = startX + BEAT_SIZES*(i + 1) + deltaX*2 + '%';
+					let deltaY;
+					if (j % 2 == 0) {
+						deltaY = SPACE_SIZE/2 * (j < POSSIBLE_NOTES_COUNT / 2 ? 1 : -1);
+					} else {
+						deltaY = 0;
+					}
+					CREATOR.create.svg('line', {x1: x1, y1: y1 + deltaY + '%', x2: x2, y2: y1 + deltaY + '%', stroke: 'black', 'stroke-width': LINE_STROKE_WIDTH, name: 'extra'}, svg);
+				}
+				group.remove();
+				svg.appendChild(group);
 			}
 		}
 		NOTES = svg.querySelectorAll('g');
+		EXTRA_LINES = svg.querySelectorAll('line[name="extra"]');
 	}
 
 	function window_HashChange(event) {
